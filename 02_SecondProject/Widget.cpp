42416ae10c1b82a893f4b0cb24539372334d5ff9
mr.huangjian <mr.huangjian@foxmail.com>
2021-07-22 00:54:52 +0800

    lambda

--- 02_SecondProject/Widget.cpp
+++ 02_SecondProject/Widget.cpp
@@ -40,6 +40,48 @@ Widget::Widget(QWidget *parent)
 
     // 断开连接(把connect换成disconnect)
     // disconnect(button, &QPushButton::clicked, teacher, hungryVoidSignal);
+
+
+    /**
+     * lambda 匿名函数
+     * = 表示局部变量值传递
+     * & 表示局部变量引用传递
+     *
+     * mutable 放在参数()后面
+     * 表示值传递过来的变量，可以在匿名函数内修改值，且只在匿名函数内有效，不影响变量本身的值
+     * 即修改的是拷贝，不是本体
+     * 可以作为 connect 的槽函数等
+     */
+
+    [=]()mutable {
+        button->setText("新标题");
+    }();
+
+    int ret = [] () -> int {
+        return 0;
+    }();
+
+    connect(button, &QPushButton::clicked, this, [=](){
+        this->close();
+    });
+
+    connect(button, &QPushButton::clicked, [=](){
+        emit teacher->hungry("tomato");
+    });
+
+
+    /*
+    Lambda 函数对象参数有以下形式：
+    1. []，没有使用任何函数对象参数。
+    2. [=]，函数体内可以使用Lambda所在作用范围内所有可见的局部交量（包括Lambda所在类的this),并且是值传递方式（相当于编译器自动为我们按值传递了所有局部变量）。
+    3. [&]，函数体内可以使用Lambda所在作用范围内所有可见的局部变量（包括Lambda所在类的this),并且是引用传递方式（相当于编译器自动为我们按引用传递了所有局部变量）。
+    4. [this]，函数体内可以使用Lambda所在类中的成员变量。
+    5. [a]，将a按值进行传递。按值进行传递时，函数体内不能修改传递进来的a的拷贝，因为默认情况下函数是const的。要修改传递进来的a的拷贝，可以添加mutable修饰符。
+    6. [&a]，将a按引用进行传递。
+    7. [a,&b]，将a按值进行传递，b按引用进行传递。
+    8. [=,&a,&b]，除a和b按引用进行传递外，其他参数都按值进行传递。
+    9. [&,a,b]，除a和b按值进行传递外，其他参数都按引用进行传递。
+     */
 }
 
 void Widget::overClass() {
